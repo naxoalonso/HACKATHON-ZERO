c5157237d137b0010eafa76136ee256ee4f10d45
naxoalonso <naxoalonso@gmail.com>
2020-05-16 12:45:54 +0200

    Kata 2 (if 2)

--- Kata2/main.py
+++ Kata2/main.py
@@ -1,3 +1,9 @@
+'''
+Una panadería vende barras de pan a 3.49€ cada una. El pan que no es el día tiene un descuento del 60%. 
+Escribe un programa que comience leyendo el número de barras vendidas que no son del día. Después tu 
+programa debe mostrar el precio habitual de una barra de pan, el descuento que se le hace por no ser 
+fresca y el coste final total. 
+'''
 precio = 3.49
 descuento = 1 - 0.6
 precio_con_descuento = precio * descuento 
